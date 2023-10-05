9512bba68712540f6f3be26097b646dfbb76933c
Iamneo <techadmin@iamneo.ai>
2023-10-05 08:29:16 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from "./players/list/list.component";
 const routes: Routes = [];
+
+RouterModule.forRoot([
+  { path: 'players', component: ListComponent }
+])
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PlayersModule } from './players/players.module';
+import { ListComponent } from "./players/list/list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PlayersModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'players', component: ListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
